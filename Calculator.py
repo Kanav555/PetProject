d278ae047e7f34603bddd09bd5a5fce443279cde
Kanav555 <kannav5555@gmail.com>
2022-08-15 23:44:04 +0200

    Updating the loop and removing identation errors

--- Calculator.py
+++ Calculator.py
@@ -1,10 +1,7 @@
 # Sample Calculator 
 
 # Program make a simple calculator
-<<<<<<< HEAD
 
-<<<<<<< HEAD
-=======
 # Program make a simple calculator
 
 # This function adds two numbers
@@ -14,7 +11,7 @@ def add(x, y):
 # This function subtracts two numbers
 def subtract(x, y):
     return x - y
-=======
+
 # This function multiplies two numbers
 def multiply(x, y):
     return x * y
@@ -22,9 +19,7 @@ def multiply(x, y):
 # This function divides two numbers
 def divide(x, y):
     return x / y
->>>>>>> Feature-2
 
->>>>>>> Release-1
 
 print("Select operation.")
 print("1.Add")
@@ -32,45 +27,39 @@ print("2.Subtract")
 print("3.Multiply")
 print("4.Divide")
 
+while True:
 
-
- # take input from the user
+     # take input from the user
     choice = input("Enter choice(1/2/3/4): ")
 
     # check if choice is one of the four options
     if choice in ('1', '2', '3', '4'):
         num1 = float(input("Enter first number: "))
-<<<<<<< HEAD
-        num2 = float(input("Enter second number: "))
-=======
+
         num2 = float(input("Enter second number: "))
 
-<<<<<<< HEAD
+
         if choice == '1':
             print(num1, "+", num2, "=", add(num1, num2))
 
-<<<<<<< HEAD
-        else choice == '2':
-            print(num1, "-", num2, "=", subtract(num1, num2))
->>>>>>> Release-1
-=======
+
         elif choice == '2':
             print(num1, "-", num2, "=", subtract(num1, num2))
-=======
+
 
         elif choice == '3':
             print(num1, "*", num2, "=", multiply(num1, num2))
 
-        else choice == '4':
+        elif choice == '4':
             print(num1, "/", num2, "=", divide(num1, num2))
         
         # check if user wants another calculation
         # break the while loop if answer is no
         next_calculation = input("Let's do next calculation? (yes/no): ")
+
         if next_calculation == "no":
-          break
+            break
     
     else:
         print("Invalid Input")
->>>>>>> Feature-2
->>>>>>> Release-2
+
